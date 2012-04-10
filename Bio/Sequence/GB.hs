c16a132e22bc5c20698c03bf60e167a80884549f
Boyun Tang <tangboyun@BoyunTang-Arch.localdomain>
2012-04-10 17:27:24 +0800

    first commit

---
+++ Bio/Sequence/GB.hs
@@ -0,0 +1,49 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module : 
+-- Copyright : (c) 2012 Boyun Tang
+-- License : BSD-style
+-- Maintainer : tangboyun@hotmail.com
+-- Stability : experimental
+-- Portability : ghc
+--
+-- 
+--
+-----------------------------------------------------------------------------
+module Bio.Sequence.GB
+       (
+         readGB
+       )
+       where
+
+import           Bio.Sequence.GB.Parser
+import           Bio.Sequence.GB.Types
+import           Bio.Sequence.SeqData
+import           Control.Applicative
+import           Data.Attoparsec.ByteString.Char8
+import           Data.ByteString                  (ByteString)
+import qualified Data.ByteString.Char8            as B8
+import           Data.Conduit                     (runResourceT,($$))
+import           Data.Conduit.Attoparsec          (sinkParser)
+import           Data.Conduit.Binary              (sourceFile)
+
+readGB :: FilePath -> IO [GBRecord]
+readGB fp = runResourceT $ sourceFile fp $$ sinkParser parseGBs
+
+toStr :: GBRecord -> String
+toStr = show
+
+fromByteString :: ByteString -> Maybe GBRecord
+fromByteString = maybeResult . parse (many space *> parseGB <* many space)
+  
+toSeq :: GBRecord -> Sequence a
+toSeq gb = undefined
+
+extractExons,extractUTR3',extractUTR5',extractGene,extractCDS,extractProtein :: GBRecord -> Maybe [Sequence a]
+extractExons = undefined
+extractUTR3' = undefined
+extractUTR5' = undefined
+extractGene = undefined
+extractCDS = undefined
+extractProtein = undefined
+
